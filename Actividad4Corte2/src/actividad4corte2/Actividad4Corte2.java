d94228b52997ee2a5eb546b39fa181bb6da96b55
AngelMerlinO <105680717+AngelMerlinO@users.noreply.github.com>
2022-06-29 23:39:27 -0500

    Add files via upload
    archivos con nombre de Actividad4Corte2 pero es la actividad 5

---
+++ Actividad4Corte2/src/actividad4corte2/Actividad4Corte2.java
@@ -0,0 +1,13 @@
+
+package actividad4corte2;
+
+
+
+
+public class Actividad4Corte2 {
+public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+    
+}
